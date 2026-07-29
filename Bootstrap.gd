81e7a9c24f215148f9309934708314ae46772db4
Bazzcuy <nanamigemoy@gmail.com>
2026-07-29 20:15:21 +0700

    Replace bootstrap loader with direct scene change

--- Bootstrap.gd
+++ Bootstrap.gd
@@ -4,13 +4,6 @@ extends Node2D
 
 func _ready() -> void:
     fallback_label.text = "ORBIT CLASH\nLOADING ARENA..."
-    call_deferred("_load_game")
-
-func _load_game() -> void:
-    await get_tree().process_frame
-    var packed := load("res://Main.tscn")
-    if packed == null:
-        fallback_label.text = "ORBIT CLASH\nMAIN SCENE FAILED TO LOAD"
-        return
-    var game := packed.instantiate()
-    add_child(game)
+    var err := get_tree().change_scene_to_file("res://Main.tscn")
+    if err != OK:
+        fallback_label.text = "ORBIT CLASH\nFAILED TO START\nERR %d" % err
