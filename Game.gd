b96af427192807ef0d7ca0d0fc7a42f1271de09a
Bazzcuy <nanamigemoy@gmail.com>
2026-08-08 19:55:48 +0700

    Add permanent build marker and v102 APK

--- Game.gd
+++ Game.gd
@@ -54,6 +54,7 @@ var right_stat: Label
 var footer_label: Label
 var watermark_label: Label
 var controls_label: Label
+var build_label: Label
 
 func _ready() -> void:
 	rng.randomize()
@@ -81,7 +82,9 @@ func _play_sfx(key: String, volume_shift := 0.0) -> void:
 
 func _make_ui() -> void:
 	title_label = _label("Orbit Clash MatchSim", Vector2(0, 34), Vector2(W, 48), 36, HORIZONTAL_ALIGNMENT_CENTER)
-	subtitle_label = _label("BUILD 101 - direct arena, no loading screen", Vector2(0, 82), Vector2(W, 34), 17, HORIZONTAL_ALIGNMENT_CENTER)
+	subtitle_label = _label("direct arena, no loading screen", Vector2(0, 82), Vector2(W, 34), 17, HORIZONTAL_ALIGNMENT_CENTER)
+	build_label = _label("BUILD 102 | com.orbitclash.matchsim100 | v1.0.2", Vector2(0, 120), Vector2(W, 26), 14, HORIZONTAL_ALIGNMENT_CENTER)
+	build_label.add_theme_color_override("font_color", Color(0, 0, 0, 0.62))
 	left_name = _label("", Vector2(36, 185), Vector2(300, 54), 34, HORIZONTAL_ALIGNMENT_LEFT)
 	right_name = _label("", Vector2(384, 185), Vector2(300, 54), 34, HORIZONTAL_ALIGNMENT_RIGHT)
 	left_hp = _label("", Vector2(0, 0), Vector2(72, 54), 24, HORIZONTAL_ALIGNMENT_CENTER)
